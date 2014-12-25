c24e10efbfd67fd51c7849ae9924d4e7a088fbe0
William Mahoney <wm.j.mahoney@gmail.com>
2014-12-25 08:32:20 -0500

    migrated songs_on_tapes table into database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141225125225) do
+ActiveRecord::Schema.define(version: 20141225131028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20141225125225) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "songs_on_tapes", force: true do |t|
+    t.integer "song_id",      null: false
+    t.integer "tape_id",      null: false
+    t.integer "track_number", null: false
   end
 
   create_table "tapes", force: true do |t|
