20914154e10f5c406ba20e8da382e32c6ecd4da0
William Mahoney <wm.j.mahoney@gmail.com>
2014-12-26 22:16:45 -0500

    cleaned up validations, have work to do to begin populating join table.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,7 @@
 class Song < ActiveRecord::Base
+  has_many :tapes, through: :songs_tapes
+  has_many :songs_tapes
+
   validates :title, presence: true
   validates :artist, presence: true
 end
--- app/models/tape.rb
+++ app/models/tape.rb
@@ -1,4 +1,7 @@
 class Tape < ActiveRecord::Base
+  has_many :songs, through: :songs_tapes
+  has_many :songs_tapes
+
   validates :title, presence: true
   validates :creator, presence: true
 end
